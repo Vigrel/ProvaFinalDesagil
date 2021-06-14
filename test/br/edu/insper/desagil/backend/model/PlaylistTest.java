0833a3a37e86c40c6d45f92f73865d4b659282c8
Vigrel <eller.vini@gmail.com>
2021-06-14 15:31:40 -0300

    Constroi testes pedidos pelo projeto

--- test/br/edu/insper/desagil/backend/model/PlaylistTest.java
+++ test/br/edu/insper/desagil/backend/model/PlaylistTest.java
@@ -7,28 +7,38 @@ import org.junit.jupiter.api.Test;
 
 class PlaylistTest {
 	private static double DELTA = 0.000001;
+	private Playlist playlist;
 
 	@BeforeEach
 	void setUp() {
+		playlist = new Playlist(5);
+		playlist.putRating("anitta", 1);
+		playlist.putRating("ludmilla", 2);
 	}
 
 	@Test
 	void testRoundDownToZero() {
-		assertEquals(true, false);
+		playlist.putRating("eller", 3);
+		playlist.putRating("vinicius", 3);
+		assertEquals(2.0, playlist.averageRatings(), DELTA);
 	}
 
 	@Test
 	void testRoundUpToHalf() {
-		assertEquals(true, false);
+		playlist.putRating("eller", 1);
+		assertEquals(1.5, playlist.averageRatings(), DELTA);
 	}
 
 	@Test
 	void testRoundDownToHalf() {
-		assertEquals(true, false);
+		playlist.putRating("eller", 2);
+		assertEquals(1.5, playlist.averageRatings(), DELTA);
 	}
 
 	@Test
 	void testRoundUpToOne() {
-		assertEquals(true, false);
+		playlist.putRating("eller", 1);
+		playlist.putRating("vinicius", 3);
+		assertEquals(2.0, playlist.averageRatings(), DELTA);
 	}
 }
--- test/br/edu/insper/desagil/backend/model/TrackTest.java
+++ test/br/edu/insper/desagil/backend/model/TrackTest.java
@@ -2,66 +2,83 @@ package br.edu.insper.desagil.backend.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TrackTest {
+	private Track track;
+	private Artist artist;
+	
 	@BeforeEach
 	void setUp() {
+		artist = new Artist("Anitta");
 	}
 
 	@Test
 	void testZeroSeconds() {
-		assertEquals(true, false);
+		track = new Track(artist, "Moda de Viola", 0);
+		assertEquals("0:00", track.getDurationString());
 	}
 
 	@Test
 	void testFiveSeconds() {
-		assertEquals(true, false);
+		track = new Track(artist, "Moda de Viola", 5);
+		assertEquals("0:05", track.getDurationString());
 	}
 
 	@Test
 	void testTwentyFiveSeconds() {
-		assertEquals(true, false);
+		track = new Track(artist, "Moda de Viola", 25);
+		assertEquals("0:25", track.getDurationString());
 	}
 
 	@Test
 	void testOneMinuteZeroSeconds() {
-		assertEquals(true, false);
+		track = new Track(artist, "Moda de Viola", 60);
+		assertEquals("1:00", track.getDurationString());
 	}
 
 	@Test
 	void testOneMinuteFiveSeconds() {
-		assertEquals(true, false);
+		track = new Track(artist, "Moda de Viola", 65);
+		assertEquals("1:05", track.getDurationString());
 	}
 
 	@Test
 	void testOneMinuteTwentyFiveSeconds() {
-		assertEquals(true, false);
+		track = new Track(artist, "Moda de Viola", 85);
+		assertEquals("1:25", track.getDurationString());
 	}
 
 	@Test
 	void testTwoMinutesZeroSeconds() {
-		assertEquals(true, false);
+		track = new Track(artist, "Moda de Viola", 120);
+		assertEquals("2:00", track.getDurationString());
 	}
 
 	@Test
 	void testTwoMinutesFiveSeconds() {
-		assertEquals(true, false);
+		track = new Track(artist, "Moda de Viola", 125);
+		assertEquals("2:05", track.getDurationString());
 	}
 
 	@Test
 	void testTwoMinutesTwentyFiveSeconds() {
-		assertEquals(true, false);
+		track = new Track(artist, "Moda de Viola", 145);
+		assertEquals("2:25", track.getDurationString());
 	}
 
 	@Test
 	void testOneCollaborator() {
-		assertEquals(true, false);
+		track = new CollaborationTrack(artist, Arrays.asList(new Artist("Becky G")), "Banana", 195);
+		assertEquals("Anitta (feat. Becky G)", track.getFullArtistName());
 	}
 
 	@Test
 	void testTwoCollaborators() {
-		assertEquals(true, false);
+		track = new CollaborationTrack(artist, Arrays.asList(new Artist("Ludmilla"), new Artist("Snoop Dog")), "Banana", 195);
+		assertEquals("Anitta (feat. Ludmilla, Snoop Dog)", track.getFullArtistName());
 	}
 }
